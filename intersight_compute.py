956124f885fe6db432de7a65da75303a864eb313
Larry Smith Jr <mrlesmithjr@gmail.com>
2020-08-27 17:46:10 -0400

    First commit of beginning of managing Intersight compute resource

---
+++ intersight_compute.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python
+"""
+    intersight_compute.py -  provides a class to support Cisco Intersight
+    interactions
+
+    author: Larry Smith Jr. (mrlesmithjr@gmail.com)
+"""
+
+import json
+import argparse
+import requests
+
+from intersight_auth import IntersightAuth
+
+# Intersight REST API Base URL
+BURL = 'https://www.intersight.com/api/v1'
+
+HEADERS = {'Accept': 'application/json',
+           'Content-Type': 'application/json'}
+
+
+def cli_args():
+    """
+    Provides CLI argument options
+
+    action: Action to take using API
+
+    apiKeyId: User's API key ID for auth
+
+    claimId: The short lived claim ID to use for claiming
+
+    deviceID: The device ID which will be used for claiming
+
+    secretKeyFileName: Path to user's API secret key file
+    """
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('action', choices=['physical', 'rackUnits'])
+    parser.add_argument('--secretKeyFileName')
+    parser.add_argument('--apiKeyId')
+
+    args = parser.parse_args()
+
+    return args
+
+
+def physical(**kwargs):
+    """Returns a JSON list of physical compute summaries"""
+
+    auth = kwargs['auth']
+
+    url = f'{BURL}/compute/PhysicalSummaries'
+    response = requests.request('GET', url, auth=auth)
+
+    print(json.dumps(response.json()))
+
+
+def rack_units(**kwargs):
+    """Returns a JSON list of rack units"""
+
+    auth = kwargs['auth']
+
+    url = f'{BURL}/compute/RackUnits'
+    response = requests.request('GET', url, auth=auth)
+
+    print(json.dumps(response.json()))
+
+
+def main():
+    """Main execution"""
+
+    args = cli_args()
+
+    auth = IntersightAuth(
+        secret_key_filename=args.secretKeyFileName,
+        api_key_id=args.apiKeyId
+    )
+
+    action_lookup = {'rackUnits': rack_units, 'physical': physical}
+    action = action_lookup[args.action]
+
+    action(args=args, auth=auth)
+
+
+if __name__ == "__main__":
+    main()
