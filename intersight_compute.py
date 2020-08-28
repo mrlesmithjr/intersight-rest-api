0a47fda161c23a027a5fd7740010124878109094
Larry Smith Jr <mrlesmithjr@gmail.com>
2020-08-28 17:12:07 -0400

    Added functionality to assign management mode of compute

--- intersight_compute.py
+++ intersight_compute.py
@@ -8,6 +8,7 @@
 
 import json
 import argparse
+import sys
 import requests
 
 from intersight_auth import IntersightAuth
@@ -31,13 +32,59 @@ def cli_args():
     """
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('action', choices=['blades', 'physical', 'rackUnits'])
-    parser.add_argument('--secretKeyFileName')
+    parser.add_argument('action', choices=[
+                        'assignManagementMode', 'blades', 'physical',
+                        'rackUnits'])
     parser.add_argument('--apiKeyId')
+    parser.add_argument('--managementMode',
+                        choices=['Intersight', 'IntersightStandalone', 'UCSM'])
+    parser.add_argument('--secretKeyFileName')
 
     args = parser.parse_args()
 
     return args
+
+
+def assign_mgmt_mode(**kwargs):
+    """Assign compute management mode"""
+
+    auth = kwargs['auth']
+    args = kwargs['args'].__dict__
+
+    mgmt_mode = args['managementMode']
+
+    if mgmt_mode is None:
+        print('--managementMode should not be None')
+        sys.exit(1)
+
+    url = f'{BURL}/compute/PhysicalSummaries'
+    response = requests.request('GET', url, auth=auth)
+
+    json_results = response.json()['Results']
+    for item in json_results:
+        moid = item['Moid']
+        obj_type = item['SourceObjectType']
+
+        if obj_type == 'compute.Blade':
+            url = f'{BURL}/compute/Blades/{moid}'
+        elif obj_type == 'compute.RackUnit':
+            url = f'{BURL}/compute/RackUnits/{moid}'
+        else:
+            pass
+
+        payload = {"ManagementMode": mgmt_mode}
+
+        response = requests.request(
+            'POST', url, auth=auth, headers=HEADERS, data=json.dumps(payload))
+
+        if response.status_code == 200:
+            results = {'status_code': response.status_code,
+                       'moid': moid, 'name': item['Name'],
+                       'obj_type': obj_type,
+                       'mgmt_mode': mgmt_mode, 'serial': item['Serial']}
+            print(json.dumps(results))
+        else:
+            print(json.dumps(response.json()))
 
 
 def blades(**kwargs):
@@ -83,7 +130,7 @@ def main():
         api_key_id=args.apiKeyId
     )
 
-    action_lookup = {'blades': blades,
+    action_lookup = {'assignManagementMode': assign_mgmt_mode, 'blades': blades,
                      'rackUnits': rack_units, 'physical': physical}
     action = action_lookup[args.action]
 
